d72410364dc124420a9253778783e2194f149222
Caspar Stanley <casparstan@gmail.com>
2022-03-07 13:09:37 +0100

    Tester script and test scenes for each of us

---
+++ Flagmingo/Assets/_Scripts/Tester.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log($"({i}) Zlatan");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
